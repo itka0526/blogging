4f50fd138da4515b66293e623dd7ade46287cb2f
itka0526 <itka0526@gmail.com>
2022-02-10 20:34:23 +0800

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const cors = require("cors");
 
-app.get("/posts", cors(), (_, res) => {
-    res.send("good");
+app.use(express.static("public"));
+app.use(express.json());
+
+const admin = ["holygod", "itgeltissmart"];
+const db = [
+    {
+        title: "first post",
+        date: "1234",
+        body: "My name is Itgelt and I am blogging about my blah blah blah ...",
+    },
+];
+app.get("/posts", (_, res) => {
+    res.json(db);
 });
+
+app.post("/newpost", cors({ origin: "*" }), (req, res) => {
+    if (req.query.username === admin[0] && req.query.password === admin[1]) {
+        db.push(req.body);
+        res.send("-------published-------");
+    } else {
+        res.json({ title: "...", date: "...", body: "..." });
+    }
+});
+
+app.listen(3000);
